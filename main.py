08f86709c8b3df5c9e59781c7b42b5f0dfc0e227
darleisantossoares <darleisoares@icloud.com>
2019-10-13 19:55:21 -0300

    bot de busca de promocoes

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+import requests
+
+from bs4 import BeautifulSoup
+
+WEBSITE_URL = 'https://www.hardmob.com.br/forums/407-Promocoes'
+
+def get_promocoes(event, context):
+
+    r = requests.get(WEBSITE_URL)
+
+    if not r.status_code == requests.codes.ok:
+        raise
+
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    div_links = soup.select('#threadlist')[0]
+
+    links = div_links.select('.title')
+
+    i = 0
+    for link in links:
+        if i > 1:
+            print(link.text, ' -> ', 'https://www.hardmob.com.br/' + link['href'])
+        i = i+1
+
+
+
+if __name__ == '__main__':
+    get_promocoes()
